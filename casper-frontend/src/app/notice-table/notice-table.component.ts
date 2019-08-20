850227ac190a26f165d513ea3795329cae140de1
Anderson Fellipe de Vasconcelos Vidal <anderson.vidal@ccc.ufcg.edu.br>
2019-08-19 21:38:23 -0300

    pagination table

--- casper-frontend/src/app/notice-table/notice-table.component.ts
+++ casper-frontend/src/app/notice-table/notice-table.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, ViewChild } from '@angular/core';
 import { Notice } from '../models/notice';
 import { NoticeAPIService } from '../services/notice-api.service'
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-notice-table',
@@ -11,17 +13,20 @@ export class NoticeTableComponent implements OnInit, OnChanges {
 
   constructor(private _api: NoticeAPIService) { }
 
-  displayedColumns: string[] = ['_id', 'title', 'topic', 'description', 'notice_url', 'image_url', 'actions'];
-  dataSource: any[];
+  displayedColumns: string[] = ['title', 'topic', 'description', 'notice_url', 'image_url', 'actions'];
+  dataSource;
   isLoadingResults;
   @Input() newData;
 
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
   ngOnInit() {
     this._api.getNotices().subscribe(res => {
-      this.dataSource = res;
+      this.dataSource = new MatTableDataSource<any>(res);
+      this.dataSource.paginator = this.paginator;
       console.log(this.dataSource);
       for (let index = 0; index < this.dataSource.length; index++) {
-        this.dataSource[index].id = index + 1;
+        this.dataSource[index].filteredData.id = index + 1;
       }
       this.isLoadingResults = false;
     }, err => {
@@ -33,6 +38,4 @@ export class NoticeTableComponent implements OnInit, OnChanges {
   ngOnChanges(newData) {
     this.ngOnInit();
   }
-
-
 }
